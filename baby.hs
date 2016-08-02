ed9a3e9600036dee945e57f6a1441daf1368afba
pukeko-eko-eko <pukeko-eko-eko@users.noreply.com>
2016-08-03 11:24:25 +1200

    first commit

---
+++ baby.hs
@@ -0,0 +1,269 @@
+-- Learn You a Haskell for Great Good!
+
+-- Ch.1
+
+doubleMe x = x + x
+
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber x = if x > 100
+			then x
+			else x*2
+
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+
+-- Ch.2
+
+addThree :: Int -> Int -> Int -> Int
+addThree x y z = x + y + z
+
+myFactorial :: Integer -> Integer
+myFactorial n = product [1..n]
+
+circumference :: Float -> Float
+circumference r = 2 * pi * r
+
+circumference' :: Double -> Double
+circumference' r = 2 * pi * r
+
+-- Ch.3
+
+lucky :: (Integral a) => a -> String
+lucky 7 = "LUCKY NUMBER SEVEN!"
+lucky x = "Sorry, you're out of luck, pal!"
+
+sayMe :: (Integral a) => a -> String
+sayMe 1 = "One!"
+sayMe 2 = "Two!"
+sayMe 3 = "Three!"
+sayMe 4 = "Four!"
+sayMe 5 = "Five!"
+sayMe x = "Not between 1 and 5"
+
+factorial :: (Integral a) => a -> a
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+addVectors1 :: (Num a) => (a, a) -> (a, a) -> (a, a)
+addVectors1 a b = (fst a + fst b, snd a + snd b)
+
+addVectors2 :: (Num a) => (a, a) -> (a, a) -> (a, a)
+addVectors2 (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+first :: (a, b, c) -> a
+first (x, _, _) = x
+
+second :: (a, b, c) -> b
+second (_, y, _) = y
+
+third :: (a, b, c) -> c
+third (_, _, z) = z
+
+head' :: [a] -> a
+head' [] = error "Can't call head on an empty list, dummy!"
+head' (x:_) = x
+
+tell :: (Show a) => [a] -> String
+tell [] = "The list is empty"
+tell (x:[]) = "The list has one element: " ++ show x
+tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y
+tell (x:y:_) = "This list is long. The first two elements are: " ++ show x ++ " and " ++ show y
+
+length' :: (Num b) => [a] -> b
+length' [] = 0
+length' (_:xs) = 1 + length' xs
+
+sum' :: (Num a) => [a] -> a
+sum' [] = 0
+sum' (x:xs) = x + sum' xs
+
+capital :: String -> String
+capital "" = "Empty string, whoops!"
+capital all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
+
+bmiTell :: (RealFloat a) => a -> String
+bmiTell bmi
+	| bmi <= 18.5 = "You're underweight, you emo, you!"
+	| bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
+	| bmi <= 30.0 = "You're fat! Lose some weight, fatty!"
+	| otherwise   = "You're a whale, congratulations!"
+
+bmiTell2 :: (RealFloat a) => a -> a -> String
+bmiTell2 weight height
+	| weight / height ^ 2 <= 18.5 = "You're underweight, you emo, you!"
+	| weight / height ^ 2 <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
+	| weight / height ^ 2 <= 30.0 = "You're fat! Lose some weight, fatty!"
+	| otherwise		      = "You're a whale, congratulations!"
+
+max' :: (Ord a) => a -> a -> a
+max' a b
+	| a > b = a
+	| otherwise = b
+
+myCompare :: (Ord a) => a -> a -> Ordering
+a `myCompare` b
+	| a > b		= GT
+	| a == b	= EQ
+	| otherwise	= LT
+
+bmiTell3 :: (RealFloat a) => a -> a -> String
+bmiTell3 weight height
+	| bmi <= 18.5 = "You're underweight, you emo, you!"
+	| bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"
+	| bmi <= 30.0 = "You're fat! Lose some weight, fatty!"
+	| otherwise		      = "You're a whale, congratulations!"
+	where bmi = weight / height ^ 2
+
+initials :: String -> String -> String
+initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
+	where (f:_) = firstname
+	      (l:_) = lastname
+
+cylinder :: (RealFloat a) => a -> a -> a
+cylinder r h =
+	let sideArea = 2 * pi * r * h
+	    topArea = pi * r ^2
+	in sideArea + 2 * topArea 
+
+calcBmis :: (RealFloat a) => [(a,a)] -> [a]
+calcBmis xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2]
+
+calcBmis2 :: (RealFloat a) => [(a, a)] -> [a]
+calcBmis2 xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2, bmi >= 25.0]
+
+head2' :: [a] -> a
+head2' xs = case xs of [] -> error "No head for empty lists!"
+                       (x:_) -> x
+
+describeList :: [a] -> String
+describeList xs = "The list is " ++ case xs of [] -> "empty."
+                                               [x] -> "a singleton list."
+                                               xs -> "a longer list."
+
+describeList' :: [a] -> String
+describeList' xs = "The list is " ++ what xs
+   where what [] = "empty."
+         what [x] = "a singleton list."
+         what xs = "a longer list."
+
+-- Ch.5
+
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "maximum of empty list"
+maximum' [x] = x
+maximum' (x:xs)
+    | x > maxTail = x
+    | otherwise = maxTail
+    where maxTail = maximum' xs
+
+maximum2 :: (Ord a) => [a] -> a
+maximum2 [] = error "maximum of empty list"
+maximum2 [x] = x
+maximum2 (x:xs) = max x (maximum2 xs)
+
+replicate' :: (Num i, Ord i) => i -> a -> [a]
+replicate' n x
+    | n <= 0    = []
+    | otherwise = x:replicate' (n-1) x
+
+take' :: (Num i, Ord i) => i -> [a] -> [a]
+take' n _
+    | n <= 0   = []
+take' _ []     = []
+take' n (x:xs) = x : take' (n-1) xs
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+repeat' :: a -> [a]
+repeat' x = x:repeat' x
+
+zip' :: [a] -> [b] -> [(a,b)]
+zip' _ [] = []
+zip' [] _ = []
+zip' (x:xs) (y:ys) = (x,y):zip' xs ys
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' a [] = False
+elem' a (x:xs)
+    | a == x    = True
+    | otherwise = a `elem'` xs
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+    let smallerSorted = quicksort [a | a <- xs, a <= x]
+        biggerSorted  = quicksort [a | a <- xs, a > x]
+    in smallerSorted ++ [x] ++ biggerSorted
+
+-- Ch.6
+
+multThree :: (Num a) => a -> a -> a -> a
+multThree x y z = x * y * z
+
+compareWithHundred :: (Num a, Ord a) => a -> Ordering
+compareWithHundred = compare 100
+
+divideByTen :: (Floating a) => a -> a
+divideByTen = (/10)
+
+isUpperAlphanum :: Char -> Bool
+isUpperAlphanum = (`elem` ['A'..'Z'])
+
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys 
+
+flip' :: (a -> b -> c) -> (b -> a -> c)
+flip' f = g
+    where g x y = f y x
+
+flip2 :: (a -> b -> c) -> b -> a -> c
+flip2 f y x = f x y
+
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' f (x:xs) = f x : map' f xs
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' p (x:xs)
+    | p x       = x : filter' p xs
+    | otherwise = filter' p xs
+
+quicksort2 :: (Ord a) => [a] -> [a]
+quicksort2 [] = []
+quicksort2 (x:xs) =
+    let smallerSorted = quicksort2 (filter (<= x) xs)
+        biggerSorted = quicksort2 (filter (>x) xs)
+    in  smallerSorted ++ [x] ++ biggerSorted
+
+largestDivisible :: (Integral a) => a
+largestDivisible = head (filter p [100000,99999..])
+	where p x = x `mod` 3829 == 0
+
+chain :: (Integral a) => a -> [a]
+chain 1 = [1]
+chain n
+    | even n = n:chain (n `div` 2)
+    | odd n  = n:chain (n*3 + 1)
+
+numLongChains :: Int
+numLongChains = length (filter isLong (map chain [1..100]))
+    where isLong xs = length xs > 15
+
+numLongChains2 :: Int
+numLongChains2 = length (filter (\xs -> length xs > 15) (map chain [1..100]))
+
+addThree2 :: (Num a) => a -> a -> a -> a
+addThree2 = \x -> \y -> \z -> x + y + z
+
+flip3 :: (a -> b -> c) -> b -> a -> c
+flip3 f = \x y -> f y x
+
+
